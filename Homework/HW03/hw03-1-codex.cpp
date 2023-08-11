e9fadd35ac949d32761ab799c7fa502a956e1f99
Mekha1104 <gtrcooie@gmail.com>
2023-08-11 10:21:06 +0700

    03

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -2,19 +2,18 @@
 #include <stdio.h>
 
 int main() {
-    int a = 10;
-    int b = 20, c = 30, d = 40;
+  int a = 10;
+  int b = 20, c = 30, d = 40;
 
-    printf("This is %d + %d = %d\n", a , b , a + b);
-
-    for (int i = 0; i < d - 30; i++) {
-      if (i < 30) {
-        printf("Here");
-        if (i == 8) {
-          printf("This is 8.");
-          }
-     }
-      printf("ok");
+  printf("This is %d + %d = %d\n", a , b , a + b);
+  for (int i = 0; i < d - 30; i++) {
+    if (i < 30) {
+      printf("Here");
+      if (i == 8) {
+        printf("This is 8.");
+      }
     }
+    printf("ok");
+  }
   return 0;
 }
